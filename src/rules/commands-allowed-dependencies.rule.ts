90536d802585bae098099e980ba7694d4e34e61b
pedenys <pedenys@hoppr.tech>
2025-03-14 11:36:40 +0100

    fix: a command can only depend on value objects

--- src/rules/commands-allowed-dependencies.rule.ts
+++ src/rules/commands-allowed-dependencies.rule.ts
@@ -8,33 +8,55 @@
  *
  * ```json
  * {
- *     "name": "AnEvent",
- *     "profile": "ddd/event"
- * },
- * {
- *     "name": "ACommand",
- *     "profile": "ddd/command",
- *     "depends_on": {
- *         "AnEvent": [
- *             "field"
- *         ]
- *     }
- * }
+ *    "dragees": [
+ *        {
+ *            "name": "AnEvent",
+ *            "profile": "ddd/event"
+ *        },
+ *        {
+ *            "name": "ACommand",
+ *            "profile": "ddd/command",
+ *            "depends_on": {
+ *                "AnEvent": ["field"]
+ *            }
+ *        }
+ *    ],
+ *    "result": {
+ *        "pass": false,
+ *        "errors": [
+ *            {
+ *                "drageeName": "ACommand",
+ *                "message": "This command must only have a dependency of type \"ddd/value_object\"",
+ *                "ruleId": "ddd/commands-allowed-dependencies"
+ *            }
+ *        ]
+ *    }
+ *}
  * ```
  * Example of correct dragees for this rule:
  *
  * ```json
  * {
- *     "name": "AnAggregate",
- *     "profile": "ddd/aggregate"
- * },
- * {
- *     "name": "ACommand",
- *     "profile": "ddd/command",
- *     "depends_on": {
- *         "AnAggregate": [
- *             "field"
- *         ]
+ *     "dragees": [
+ *         {
+ *             "name": "AValueObject",
+ *             "profile": "ddd/value_object"
+ *         },
+ *         {
+ *             "name": "AnotherValueObject",
+ *             "profile": "ddd/value_object"
+ *         },
+ *         {
+ *             "name": "ACommand",
+ *             "profile": "ddd/command",
+ *             "depends_on": {
+ *                 "AValueObject": ["field"],
+ *                 "AnotherValueObject": ["field"]
+ *             }
+ *         }
+ *     ],
+ *     "result": {
+ *         "pass": true
  *     }
  * }
  * ```
@@ -43,31 +65,39 @@
  *
  */
 import {
-    type RuleResult,
-    RuleSeverity,
-    directDependencies,
-    expectDragee
-} from '@dragee-io/type/asserter';
-import type { Dragee, DrageeDependency } from '@dragee-io/type/common';
-import { aggregateProfile, commandProfile, profileOf, profiles } from '../ddd.model.ts';
+  type RuleResult,
+  RuleSeverity,
+  directDependencies,
+  expectDragee,
+} from "@dragee-io/type/asserter";
+import type { Dragee, DrageeDependency } from "@dragee-io/type/common";
+import {
+  commandProfile,
+  profileOf,
+  profiles,
+  valueObjectProfile,
+} from "../ddd.model.ts";
 
-const assertDrageeDependency = ({ root, dependencies }: DrageeDependency): RuleResult[] =>
-    dependencies.map(dependency =>
-        expectDragee(
-            root,
-            dependency,
-            `This command must not have any dependency of type "${dependency.profile}"`,
-            dragee => profileOf(dragee, aggregateProfile)
-        )
-    );
+const assertDrageeDependency = ({
+  root,
+  dependencies,
+}: DrageeDependency): RuleResult[] =>
+  dependencies.map((dependency) =>
+    expectDragee(
+      root,
+      dependency,
+      `This command must only have a dependency of type "${valueObjectProfile}"`,
+      (dragee) => profileOf(dragee, valueObjectProfile)
+    )
+  );
 
 export default {
-    label: 'Commands Allowed Dependencies',
-    severity: RuleSeverity.ERROR,
-    handler: (dragees: Dragee[]): RuleResult[] =>
-        profiles[commandProfile]
-            .findIn(dragees)
-            .map(command => directDependencies(command, dragees))
-            .filter(dep => dep.dependencies)
-            .flatMap(dep => assertDrageeDependency(dep))
+  label: "Commands Allowed Dependencies",
+  severity: RuleSeverity.ERROR,
+  handler: (dragees: Dragee[]): RuleResult[] =>
+    profiles[commandProfile]
+      .findIn(dragees)
+      .map((command) => directDependencies(command, dragees))
+      .filter((dep) => dep.dependencies)
+      .flatMap((dep) => assertDrageeDependency(dep)),
 };
--- src/test/ddd/commands-rules/commands-rules.spec.ts
+++ src/test/ddd/commands-rules/commands-rules.spec.ts
@@ -1,18 +1,18 @@
-import { describe } from 'bun:test';
-import { ruleFailed, rulePassed } from '../../test_utils';
+import { describe } from "bun:test";
+import { ruleFailed, rulePassed } from "../../test_utils";
 
-describe('Command rules', () => {
-    const COMMAND_FOLDER = './ddd/commands-rules/';
-    const COMMANDS_ALLOWED_DEPENDENCIES_RULE = 'commands-allowed-dependencies';
+describe("Command", () => {
+  const COMMAND_FOLDER = "./ddd/commands-rules/";
+  const COMMANDS_ALLOWED_DEPENDENCIES_RULE = "commands-allowed-dependencies";
 
-    describe('Should only contains Aggregate', () => {
-        rulePassed(
-            `${COMMAND_FOLDER}${COMMANDS_ALLOWED_DEPENDENCIES_RULE}-rule/rule-passed.json`,
-            COMMANDS_ALLOWED_DEPENDENCIES_RULE
-        );
-        ruleFailed(
-            `${COMMAND_FOLDER}${COMMANDS_ALLOWED_DEPENDENCIES_RULE}-rule/rule-failed.json`,
-            COMMANDS_ALLOWED_DEPENDENCIES_RULE
-        );
-    });
+  describe("can only depend on value objects", () => {
+    rulePassed(
+      `${COMMAND_FOLDER}${COMMANDS_ALLOWED_DEPENDENCIES_RULE}-rule/rule-passed.json`,
+      COMMANDS_ALLOWED_DEPENDENCIES_RULE
+    );
+    ruleFailed(
+      `${COMMAND_FOLDER}${COMMANDS_ALLOWED_DEPENDENCIES_RULE}-rule/rule-failed.json`,
+      COMMANDS_ALLOWED_DEPENDENCIES_RULE
+    );
+  });
 });
